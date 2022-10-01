3b3e887e1149f45c217cd7c9c2be337cb0f48d65
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2022-10-01 11:14:48 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 1968 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -2,17 +2,16 @@
 max_n = 10000
 primeyn = [False, False] + [True]*(max_n-1)
 primelist = []
-
+for num in range(2, max_n):
+    if primeyn[num]:
+        primelist.append(num)
+        for i in range(num, max_n+1, num):
+            primeyn[i] = False
+            
 #골드바흐 추측 계산
 T = int(input())
 for i in range(T):
     n = int(input())
-    for num in range(2, max_n):
-        if primeyn[num]:
-            primelist.append(num)
-            for i in range(num, max_n+1, num):
-                primeyn[i] = False
-
     for i in range (int(n/2),0,-1):
         if (i in primelist) and (n-i in primelist):
             print(i, n-i)
