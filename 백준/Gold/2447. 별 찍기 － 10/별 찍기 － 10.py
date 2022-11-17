37a1e455c2053f77db3bdf6a245d55c3572abcc7
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2022-11-17 09:28:19 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 80 ms, Memory: 40188 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -0,0 +1,20 @@
+n = int(input())
+def star(s):
+    if s == 3:
+        return ['***', '* *', '***']
+    
+    arr = star(s//3)
+    stars = []
+    
+    for i in arr:
+        stars.append(i*3)
+    
+    for i in arr:
+        stars.append(i+' '*(s//3)+i)
+        
+    for i in arr:
+        stars.append(i*3)
+        
+    return stars    
+
+print('\n'.join(star(n)))
