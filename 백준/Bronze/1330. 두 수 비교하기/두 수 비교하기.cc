26eb193f27715ef4602d3972c51719e7904cca34
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-01-02 10:59:33 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -1,18 +1,16 @@
 #include <iostream>
 
 int main(){
-    int A,B;
+    int A, B;
+
     std::cin >> A >> B;
-    
-    if (A < B){ 
-       std::cout << '<'; //한글자는 char도 가능
+
+    if (A > B) {
+        std::cout << '>';
     }
-    else if (A>B)
-    {
-        std::cout << ">"; //한글자는 string도 가능
+    else if (A < B) {
+        std::cout << '<';
     }
-    else {
-        std::cout << "=="; //string은 ""
-    }
-    return 0;
+    else {std::cout << "==";}
+
 }
