d5f471acbfbcb85d5bc96a9cbc6e4dca14afbc36
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-01-02 16:14:50 +0900

    [Bronze I] Title: 더하기 사이클, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.cc
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.cc
@@ -1,20 +1,20 @@
 #include <iostream>
-using namespace std;
 
 int main(){
-    int N;
-    cin >> N;
-    int number = N; //alias가 아니겠찌? 얕은 copy?
-    int count = 0;
+    int N, units, tens, newU, newT,newN=-1,cycle=0;
+    std::cin >> N;
+    units = N%10;
+    tens = N/10;
 
-    while(true){
-    int one = N/10;
-    int two = N%10;
-    int three = one+two;
+    while (newN != N){
+        cycle += 1;
+        newT = units;
+        newU = (tens+units)%10;
+        newN = newT*10 + newU;
 
-    N = (two*10)+(three%10);
-    count ++;
-    if (number==N){break;}
+        tens = newT;
+        units = newU;
     }
-    cout << count << endl;
+    std::cout << cycle;
+
 }
