b1d34d7d7fb10a5d54a58fcf8ebf57bd92996ab7
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-01-02 16:00:14 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -1,15 +1,13 @@
 #include <iostream>
-using namespace std;
 
 int main(){
     int A,B;
-    A = 1;
-    B = 1;
-    while(1){
-        cin >> A >> B;
-        if(A==0 && B==0){
-        break;
+    while(1){    
+        std::cin >> A >> B;
+        
+        if (A==0 && B==0){
+            break;
         }
-        cout << A+B << '\n';
+        std::cout << A+B << '\n';
     }
 }
