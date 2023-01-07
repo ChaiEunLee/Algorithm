966565099b5deccd3e61f8ec84a863b46f111a82
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-01-07 15:13:37 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.cc
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -4,17 +4,16 @@ using namespace std;
 int main(){
     int N;
     cin >> N;
-    float score[N];
-    int maxScore = 0;
-    float totalScore = 0;
-    for(int i=0;i<N;i++){
-        cin >> score[i];
-        if (score[i] > maxScore){
-            maxScore = score[i];
+    float scoresum =0;
+    int scoreMax = 0;
+    float scoreList[N];
+
+    for (int i=0; i<N; i++){
+        cin >> scoreList[i];
+        if (scoreMax < scoreList[i]){
+            scoreMax = scoreList[i];
         }
+        scoresum += scoreList[i];
     }
-    for (int i=0;i<N;i++){
-        totalScore += score[i]/maxScore*100;
-    }
-    cout << totalScore/N;
+    cout << scoresum*100/(N*scoreMax);
 }
