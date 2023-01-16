830692718d01eaedaabb91a3be652d5c30cbba79
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-01-16 15:04:34 +0900

    [Bronze II] Title: 수 정렬하기, Time: 76 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,16 +1,8 @@
-#selection sort
-sortlist = []
 N = int(input())
-for num in range(N):
-    n = int(input())
-    sortlist.append(n)
-    
-for i in range(len(sortlist)):
-    minval = sortlist[i]
-    for j in range(i,len(sortlist)):
-        if sortlist[j] < minval:
-            minval = sortlist[j]
-            minidx = j
-    if sortlist[i] > minval:
-        sortlist[i], sortlist[minidx] = sortlist[minidx], sortlist[i]
-    print(sortlist[i])
+numlist = []
+for i in range(N):
+    numlist.append(int(input()))
+
+numlist_sorted = sorted(numlist)
+for i in range(N):
+    print(numlist_sorted[i])
