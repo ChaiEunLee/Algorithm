86857d2fa74be88495b257d22ce93309bf6cc59f
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-01-16 15:44:09 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9292 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,14 +1,17 @@
-# 10989번
-
+#이렇게 풀었더니 런타임 에러.
 import sys
 
-n = int(sys.stdin.readline())
-num_list = [0] * 10001
+#N = int(input())
+N = int(sys.stdin.readline())
+sortlist = [0]*10001 #입력가능한 수는 10,000 이하임. 그래서 굳이 10,000,000 array를 안 만들어도 됨.
 
-for _ in range(n):
-    num_list[int(sys.stdin.readline())] += 1
-
+for i in range(N):
+#    num = int(input())
+#    num = int(sys.stdin.readline())
+    sortlist[int(sys.stdin.readline())] += 1
+    
 for i in range(10001):
-    if num_list[i] != 0:
-        for j in range(num_list[i]):
+    if (sortlist[i] != 0): #if (sortlist[i] > 0): 이렇게 해도 안되고 
+        for j in range(sortlist[i]):
             print(i)
+#        print((str(i)+'\n')*sortlist[j],end='')
