497f926d454ff4d97e1d5eb650283423c1798317
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-01-16 12:33:17 +0900

    [Silver V] Title: 색종이, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,20 +1,14 @@
-#3.색종이
-#흰색 도화지 100*100
-#검은색 색종이 각각 10*10
-#입력값 : 왼쪽 변/하단 변과 색종이의 거리 100-L, 100-R
-
 N = int(input())
-total = 0
 paper = [[0]*100 for i in range(100)]
 
-#검은색이 닿으면 1로 변경. 이미 1이면 넘어감.
-for n in range(N):
-    L,R = map(int, input().split())
-    for i in range(L,L+10): #0~L-1
-        for j in range(R, R+10): #0~R-1
-            if paper[i][j] == 0:
-                paper[i][j] = 1
-                
+for i in range(N):
+    x,y = map(int, input().split())
+    for j in range(10):
+        for k in range(10):
+            paper[x+j][y+k] = 1
+total = 0
 for i in range(100):
-    total += sum(paper[i])
+    for j in range(100):
+        total += paper[i][j]
+        
 print(total)
