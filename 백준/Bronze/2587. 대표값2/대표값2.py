6bcde364d30a5a16570b309a4244ae8127278788
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-01-16 15:11:32 +0900

    [Bronze II] Title: 대표값2, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.py
+++ 백준/Bronze/2587. 대표값2/대표값2.py
@@ -1,8 +1,7 @@
 numlist = []
-for _ in range(5):
+for i in range(5):
     numlist.append(int(input()))
-#   numlist.append(int(sys.stdin.readline()))
-
+    
 numlist.sort()
 print(int(sum(numlist)/len(numlist)))
 print(numlist[2])
