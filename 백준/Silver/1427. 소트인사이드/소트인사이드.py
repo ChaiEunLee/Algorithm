a69804d7d5ef25c7e25abe9244511d095264c60f
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-01-16 16:39:22 +0900

    [Silver V] Title: 소트인사이드, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,13 +1,9 @@
-number = input()
-numlist = [0]*10
-for i in range(len(number)):
-    new = int(number[i])
-    for j in range(len(numlist)):
-        if numlist[j] <= new: #내림차순
-            #한칸씩 뒤로 밀기 
-            numlist[j+1:len(numlist)] = numlist[j:len(numlist)-1]#j~len(numlist)-1
-            numlist[j] = new
-#            print(j, numlist, new)
-            break
-for i in range(len(number)):
-    print(str(numlist[i]),end='')
+strinput = input()
+nlist=[]
+for i in range(len(strinput)):
+    nlist.append(int(strinput[i]))
+    
+nlist.sort()
+
+for j in range(len(strinput)-1,-1,-1):
+    print(nlist[j], end='')
