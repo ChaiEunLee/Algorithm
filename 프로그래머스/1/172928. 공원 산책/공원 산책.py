c771e96b7335f64695d58466a668a2988892def7
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-03-20 14:10:07 +0900

    [level 1] Title: 공원 산책, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -0,0 +1,43 @@
+
+def solution(park, routes):
+    parks = []
+    obstacles = []
+    for i in range(len(park)):
+        parks.append(list(park[i]))
+        if 'S' in list(park[i]):
+            for j in range(len(parks[i])):
+                if parks[i][j] == 'S':
+                    x,y = i,j
+        if 'X' in list(park[i]):
+            for j in range(len(parks[i])):
+                if parks[i][j] == 'X':
+                    obstacles.append([i,j])
+    #print(x,y)
+    # E
+    # 동남서북이라서 +1, 거꾸로 가려면 -1
+    dx = [0,1,0,-1]
+    dy = [1,0,-1,0]
+    for tmp in routes:
+        route = list((tmp.split(' ')[0],int(tmp.split(' ')[1])))
+        if route[0] == 'E':
+            i = 0
+        elif route[0] == 'S':
+            i = 1
+        elif route[0] == 'W':
+            i = 2
+        else:
+            i = 3
+
+        if 0<=x+dx[i]*route[1]<len(parks) and 0<=y+dy[i]*route[1]<len(parks[0]):
+            nx,ny = x,y
+            for _ in range(route[1]): #다 돌면서 
+                nx = nx + dx[i]
+                ny = ny + dy[i]
+                if parks[nx][ny] == 'X':
+                    nx, ny = x,y #원상복구
+                    break
+            x = nx
+            y = ny
+        #print(x,y)
+    answer = [x,y]
+    return answer
