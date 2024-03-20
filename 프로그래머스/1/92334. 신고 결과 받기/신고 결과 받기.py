ad88e375e03fc4b397267878e8d85299f76d3b2e
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-03-20 13:40:37 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,25 +1,14 @@
-import collections
 def solution(id_list, report, k):
     # 내가 신고한 계정이 정지되면 메일 발송
     # 한명이 여러번 신고해도 1번으로 카운트
     # k번이상 신고되면 정지
     # "신고자 신고당한"
-    receive = [0]*len(id_list)
     answer = [0]*len(id_list)
-    id_dict = {}
-    for i, name in enumerate(id_list):
-        id_dict[name] = i
-    #print(id_dict)
-    # key : receive, value : 신고자
-    graph = collections.defaultdict(list)
-    for tmp in report:
-        user, recv = tmp.split(' ')
-        if id_dict[user] not in graph[id_dict[recv]]:
-            graph[id_dict[recv]].append(id_dict[user])
-            receive[id_dict[recv]] += 1
-    #print(graph, receive)
-    for j in range(len(receive)):
-        if receive[j] >= k:
-            for user in graph[j]:
-                answer[user] += 1
+    reports = {x: 0 for x in id_list}
+
+    for r in set(report):
+        reports[r.split(' ')[1]] += 1
+    for r in set(report):
+        if reports[r.split(' ')[1]] >= k:
+            answer[id_list.index(r.split(' ')[0])] += 1
     return answer
