709c12755a958152259d3c89f95c80d3f01e54c9
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-03-21 18:52:26 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.73 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,13 +1,5 @@
+import collections
 def solution(participant, completion):
-    comp_dict = {}
-    for c in completion:
-        if c in comp_dict:
-            comp_dict[c] += 1
-        else:
-            comp_dict[c] = 1
-        
-    for p in participant:
-        if p not in comp_dict or comp_dict[p] == 0:
-            return p
-        else:
-            comp_dict[p] -= 1
+    answer = collections.Counter(participant) - collections.Counter(completion)
+
+    return list(answer.keys())[0]
