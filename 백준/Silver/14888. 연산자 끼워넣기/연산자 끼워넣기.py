ce44ad055fd6d872cc0119097574f3f5ab8de058
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-03-22 13:41:35 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -3,25 +3,30 @@ input = sys.stdin.readline
 
 N = int(input())
 number = list(map(int, input().split()))
-add, sub, mul, div = map(int, input().split())
+operator = list(map(int, input().split()))
 
-max_result = -int(1e9)
-min_result = int(1e9)
+def DFS(add, sub, mul, div, num, idx):
+    #print(add, sub, mul, div, num, idx)
+    global max_val
+    global min_val
 
-def dfs(add, sub, mul, div, sum, idx):
-    global max_result, min_result # declare global variable
     if idx == N:
-        max_result = max(max_result, sum)
-        min_result = min(min_result, sum)
-        return
-    if add:
-        dfs(add-1, sub, mul, div, sum+number[idx], idx+1)
-    if sub:
-        dfs(add, sub-1, mul, div, sum-number[idx], idx+1)
-    if mul:
-        dfs(add, sub, mul-1, div, sum*number[idx], idx+1)
-    if div:
-        dfs(add, sub, mul, div-1, int(sum/number[idx]), idx+1)
-dfs(add, sub, mul, div, number[0], 1)
-print(max_result)
-print(min_result)
+        max_val = max(max_val, num)
+        min_val = min(min_val, num)
+
+    if add>0:
+        DFS(add-1, sub, mul, div, num+number[idx], idx+1)
+     
+    if sub>0:
+        DFS(add, sub-1, mul, div, num-number[idx], idx+1)
+
+    if mul>0:
+        DFS(add, sub, mul-1, div, num*number[idx], idx+1)
+
+    if div>0:
+        if number[idx] != 0:
+            DFS(add, sub, mul, div-1, int(num/number[idx]), idx+1)
+max_val =  -int(1e9)
+min_val = int(1e9)
+DFS(operator[0], operator[1], operator[2], operator[3], number[0] , 1)
+print(max_val, min_val)
