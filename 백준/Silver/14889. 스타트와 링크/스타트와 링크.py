6a9bc1ec4d26723a50ec944015c7be705e83115d
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-03-22 14:21:39 +0900

    [Silver I] Title: 스타트와 링크, Time: 5456 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,36 +1,25 @@
-import sys
-input = sys.stdin.readline
+from itertools import combinations
 N = int(input())
-board = []
+S = []
 for _ in range(N):
-    board.append(list(map(int, input().split())))
-    
-visited = [False for _ in range(N)]
-INF = 2147000000
-res = INF
+    S.append(list(map(int, input().split())))
 
-
-
-def dfs(L,idx):
-    global res
-    #print("L,idx: ", L, idx)
-    if L == N//2: #if length == L//2
-        A = 0
-        B = 0
-        # i,j 조합 잘 찾아서 계산
-        for i in range(N):
-            for j in range(N):
-                if visited[i] and visited[j]:
-                    A += board[i][j]
-                elif not visited[i] and not visited[j]:
-                    B += board[i][j]
-        res = min(res, abs(A-B))
-        return 
-    # 0~N-1 돌면서 
-    for i in range(idx, N):
-        if not visited[i]:
-            visited[i] = True
-            dfs(L+1, i+1) #dfs(Length, index), length +1씩 늘어나고, index는 (1,2,3), (1,3,4), (1,4,5), (1,4,6), (2,3,4), (2,4,5), ... 이런식으로 앞에서부터 누적으로 늘어남.
-            visited[i] = False
-dfs(0,0)
-print(res)
+all = set(range(N))
+done = 0
+minval = 100*10
+for tmp in combinations(range(N),N//2):
+    first = set(tmp)  
+    second = all-first
+    #print(first, second, done)
+    firstscore = 0
+    secondscore = 0
+    for elem in combinations(first,2):
+        firstscore += S[elem[0]][elem[1]]
+        firstscore += S[elem[1]][elem[0]]
+    for elem in combinations(second,2):
+        secondscore += S[elem[0]][elem[1]]
+        secondscore += S[elem[1]][elem[0]]
+    if abs(firstscore-secondscore) < minval:
+        minval = abs(firstscore-secondscore)
+    #print(firstscore, secondscore, minval)
+print(minval)
