027dcc5be75f2b332c5ee6364a3e46bbe6e97196
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-03-22 14:33:59 +0900

    [Bronze II] Title: 시험 감독, Time: 636 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.py
+++ 백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -1,16 +1,14 @@
-import sys
-input = sys.stdin.readline
 N = int(input())
 A = list(map(int, input().split()))
 B,C = map(int, input().split())
+answer = len(A)
+for num in A:
+    # 총 감독관
+    num -= B
+    # 부 감독관
+    if num > 0:
+        answer += (num//C)+1
+        if num%C == 0:
+            answer -= 1
 
-result = 0
-for a in A:
-    a = a-B #총감독관
-    result += 1 
-    if a>0:
-        result += a//C
-        if a%C>0:
-            result += 1
-print(result)
-
+print(answer)
