6909a718bf680e790b8e52ec3d5d35aa15d89f6a
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-04-13 14:42:02 +0900

    [Gold III] Title: 아기 상어, Time: 144 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,49 +1,43 @@
-from collections import deque
-# 엄마 상어에게 도움을 요청하지 않고 잡아먹을 수 있는 시간
-# 크기가 같은 물고기는 지나가기만 가능
-# 작아야 먹기 가능, 크면 못지나감
-# 자기만큼 먹어야 1증가
-# 9: 아기상어의 위치, 크기는 2
-# 가장 가까운 물고리, 
-# 위, 왼쪽 우선순위
+# NxN
+# M fish
+# 1 baby shark
+# babysharksize = 2
+# 상하좌우
+# 자기보다 큰 물고기가 있으면 지나갈수 없음.
+# 자기보다 작은 물고기만 먹을 수 있음
+# 크기가 같으면 지나갈수 있지만 먹을 수 없음
 
-import sys
-input = sys.stdin.readline
 N = int(input())
 sea = []
 for _ in range(N):
     sea.append(list(map(int, input().split())))
 
-INF = 1e9
-
-now_size = 2
-now_x, now_y = 0,0
-
 for i in range(N):
     for j in range(N):
-        if sea[i][j] == 9:
-            now_x, now_y = i,j
-            sea[now_x][now_y] = 0
+        if sea[i][j]==9:
+            shark = [i,j]
+            sea[i][j] = 0
 
 dx = [-1,0,1,0]
 dy = [0,1,0,-1]
 
+INF = 1e9
+sharksize = 2
+
 def bfs():
-    # 값이 -1이면 도달불가하다는 의미
-    dist = [[-1]*N for _ in range(N)]
-    # 시작 위치는 도달가능하다고 보며 거리는 0
-    q = deque([(now_x, now_y)])
-    dist[now_x][now_y] = 0
+    dist = [[-1] *N for _ in range(N)]
+    x,y = shark
+    q = [(x,y)]
+    dist[x][y] = 0
     while q:
-        x,y = q.popleft()
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
+        x,y = q.pop(0)
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
             if 0<=nx<N and 0<=ny<N:
-                if dist[nx][ny] == -1 and sea[nx][ny] <= now_size:
+                if dist[nx][ny] == -1 and sea[nx][ny] <= sharksize:
                     dist[nx][ny] = dist[x][y] + 1
-                    q.append((nx,ny))
-        #print(dist, queue)
+                    q.append((nx, ny))
     return dist
 
 def find(dist):
@@ -51,28 +45,31 @@ def find(dist):
     min_dist = INF
     for i in range(N):
         for j in range(N):
-            if dist[i][j] != -1 and 1<=sea[i][j] and sea[i][j] < now_size:
+            if dist[i][j] != -1 and 1<=sea[i][j] and sea[i][j]<sharksize:
                 if dist[i][j] < min_dist:
-                    x,y = i,j
                     min_dist = dist[i][j]
+                    x,y = i,j
     if min_dist == INF:
         return None
     else:
         return x,y,min_dist
 
+
+# 더이상 먹을 수 있는 물고기가 없으면 끝
+# 먹을 수 있는 가장 가까운 물고기 찾기 (상 -> 좌)
 result = 0
 ate = 0
 
 while True:
-    value = find(bfs()) # 먹을 수 있는 물고기의 위치 찾기
+    value = find(bfs())
     if value == None:
         print(result)
         break
     else:
-        now_x, now_y = value[0], value[1]
+        shark = [value[0], value[1]]
         result += value[2]
-        sea[now_x][now_y] = 0
+        sea[shark[0]][shark[1]] = 0
         ate += 1
-        if ate >= now_size:
-            now_size += 1
+        if ate >= sharksize:
+            sharksize += 1
             ate = 0
