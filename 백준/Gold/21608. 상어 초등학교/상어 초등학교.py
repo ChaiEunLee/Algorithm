f27f3ab4340d90d22e1a7d87af0bbf4297ea66d4
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-04-13 13:06:02 +0900

    [Gold V] Title: 상어 초등학교, Time: 184 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
@@ -1,55 +1,65 @@
-n = int(input())
-data = [[0] * n for _ in range(n)]
-students = [list(map(int, input().split())) for _ in range(n**2)]
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-for student in students:
-    available = []
-
-    for i in range(n):
-        for j in range(n):
-            # 빈자리가 있다면
-            if data[i][j] == 0:
-                prefer, empty = 0, 0
-                
-                # 동서남북 방향 확인하여 
-                for k in range(4):
-                    nx = i + dx[k]
-                    ny = j + dy[k]
-                     
-                    # 범위내에 있을 때
-                    if 0 <= nx < n and 0 <= ny < n:
-                        # 좋아하는 학생이 주위에 있다면 더해준다.
-                        if data[nx][ny] in student[1:]:
-                            prefer += 1
-                            
-                        # 빈자리가 있다면 더해준다.
-                        if data[nx][ny] == 0:
+# NxN
+# 1. 좋아하는 학생이 많은 인접한 칸으로
+# 2. 1이 여러개라면 인접한 칸 중 비어있는 칸이 가장 많은 칸으로
+# 3. 2가 여러개라면 행의 번호가 작은 칸 -> 열의 번호가 작은 칸
+def search_seat(student):
+    maxlike = 0
+    queue = []
+    # 좋아하는 학생 수, 빈칸 수 탐색
+    for i in range(1,N+1):
+        for j in range(1,N+1):
+            like, empty = 0,0
+            # 빈자리 중에서
+            if board[i][j]==0:
+                # 좋아하는 학생 수, 빈 자리수 세기
+                for d in range(4):
+                    nx = i+dx[d]
+                    ny = j+dy[d]
+                    if 1<=nx<=N and 1<=ny<=N:
+                        if board[nx][ny] in prefer[student]:
+                            like += 1
+                        if board[nx][ny]==0:
                             empty += 1
+                if like > maxlike:
+                    maxlike = like
+                    queue = [[i,j,empty]]
+                elif like == maxlike:
+                    maxlike = like
+                    queue.append([i, j, empty])
+    return queue
 
-                available.append((i, j, prefer, empty))
-    # 정렬
-    available.sort(key= lambda x: (-x[2], -x[3], x[0], x[1]))
-    data[available[0][0]][available[0][1]] = student[0]
+N = int(input())
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
 
+prefer = {}
+board = [[0]*(N+1) for _ in range(N+1)]
 answer = 0
-score = [0, 1, 10, 100, 1000]
-students.sort()
+for i in range(N**2):
+    tmp = list(map(int,input().split()))
+    prefer[tmp[0]] = tmp[1:]
+    student = tmp[0]
+    queue = search_seat(student)
+    maxempty = -1
+    maxi,maxj = 0,0
+    while queue:
+        r,c,empty = queue.pop(0)
+        if empty>maxempty:
+            maxempty = empty
+            maxi,maxj = r,c
+    board[maxi][maxj] = student
 
-for i in range(n):
-    for j in range(n):
-        count = 0
-
-        for k in range(4):
-            nx = i + dx[k]
-            ny = j + dy[k]
-
-            if 0 <= nx < n and 0 <= ny < n:
-                if data[nx][ny] in students[data[i][j] - 1]:
-                    count += 1
-
-        answer += score[count]
-
+# 다 구하고 만족도 구해야함.
+answer = 0
+for i in range(1,N+1):
+    for j in range(1,N+1):
+        like = 0
+        student = board[i][j]
+        for d in range(4):
+            nx = i+dx[d]
+            ny = j+dy[d]
+            if 1<=nx<=N and 1<=ny<=N:
+                if board[nx][ny] in prefer[student]:
+                    like += 1
+        answer += int((10)**(like-1))
 print(answer)
