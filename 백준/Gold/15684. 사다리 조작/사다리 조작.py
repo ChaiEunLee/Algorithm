a4a8e8ff7eacaac80e6003be5a155be4c04a3318
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-04-13 09:39:13 +0900

    [Gold III] Title: 사다리 조작, Time: 132 ms, Memory: 110600 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.py
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.py
@@ -1,63 +1,53 @@
-def is_complete(i):
-    x, y = 0, i
-    while x < height:
-        if y - 1 >= 0 and ladders[x][y - 1]:
-            y = y - 1
-        elif y < line_cnt - 1 and ladders[x][y]:
-            y = y + 1
-        x += 1
-    if y != i:
-        return False
-    return True
+# 15684. 사다리 조작
+N,M,H = map(int,input().split())
 
+# 1개, 2개, 3개의 사다리를 추가로 놓고
+# 정답을 도출해보기
+# 2<=N<=10, 1<=H<=30, 0<=M<=(N-1)*H
+# 30*10 300C1 + 300C2 + 300C3 정도 되겠네 괜춘한듯??
+#N,M,H = 2, 1, 3
+board = [[False]*N for _ in range(H)]
+for _ in range(M):
+    a,b = map(int,input().split())
+    board[a-1][b-1] = True
 
-def backtracking(x, y, depth):
-    global answer
-    # 탈출 전에 지금 조건에서 사다리여부 체크
-    if depth >= answer:  # 정답이 될 가능성 없음
+def check():
+    wrong = 0
+    for start in range(N):
+        now = start
+        for j in range(H):
+            if board[j][now]: #now~now+1을 잇는 선
+                now += 1
+            elif now>0 and board[j][now-1]: #now-1~now를 잇는 선
+                now -= 1
+        if now!= start:
+            wrong += 1
+    return wrong
+def dfs(cnt, x, y):
+    global ans
+    wrong = check()
+    if wrong > ((ans - 1 - cnt) * 2):
         return
-
-    non_correct = 0
-    for i in range(line_cnt):
-        if not is_complete(i):
-            non_correct += 1
-    if non_correct > ((answer - 1 - depth) * 2):
-        # 하나의 사다리는 2개의 결과를 바꿀 수 있다.
-        # 지금이 유망하려면 정답보다 적은 개수로 사다리를 완성시켜야 하는데
-        # 안맞는 사다리가 (정답-1)*2개 보다 많다면 가지치기
+    elif wrong == 0:
+        ans = min(ans,cnt)
         return
-    elif non_correct == 0:
-        answer = min(answer, depth)
+    elif cnt==3 or ans <= cnt:
         return
+    for i in range(x,H):
+        # 지금 탐색하고 있는 행이면
+        if i==x:
+            now = y #지금 탐색하고 있는 열부터
+        # 새로운 행으로 넘어가면
+        else:
+            now = 0 #0열부터 탐색
+        for j in range(now, N-1):
+            if not board[i][j] and not board[i][j+1]:
+                if j>0 and board[i][j-1]:
+                    continue
+                board[i][j] = True
+                dfs(cnt+1, i, j+2)
+                board[i][j] = False
 
-    # 탈출 조건
-    if depth >= 3:
-        return
-
-    for i in range(x, height):
-        for j in range(line_cnt - 1):
-            if i == x and j <= y:  # 이번 x 줄인데 j가 지금보다 작거나 같은 경우
-                continue
-            if (j - 1 >= 0 and ladders[i][j - 1]) or (j + 1 < line_cnt - 1 and ladders[i][j + 1]) or ladders[i][j]:
-                # 양쪽에 사다리가 하나라도 있을 경우 -> 인접할 수 없다.
-                continue
-            ladders[i][j] = 1
-            backtracking(i, j, depth + 1)
-            ladders[i][j] = 0
-
-
-answer = 4
-
-line_cnt, hor_cnt, height = map(int, input().split())
-ladders = [[0 for _ in range(line_cnt - 1)] for _ in range(height)]
-
-for hor in range(hor_cnt):
-    a, b = map(lambda x: int(x) - 1, input().split())
-    ladders[a][b] = 1
-
-backtracking(0, -1, 0)
-
-if answer == 4:
-    print(-1)
-else:
-    print(answer)
+ans = 4
+dfs(0,0,0)
+print(ans if ans < 4 else -1)
